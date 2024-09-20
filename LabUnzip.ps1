5fde7d4a1ff8b30d4479a186123c5947d947c125
Ben Sippel <brs6412@rit.edu>
2024-09-20 15:17:28 -0400

    Adding powershell script

---
+++ LabUnzip.ps1
@@ -0,0 +1,46 @@
+﻿# Define path to downloads folder
+$downloadsPath = [System.IO.Path]::Combine($env:USERPROFILE, 'Downloads')
+
+# Enter the lab number
+$labNumber = Read-Host "Enter the lab number"
+
+# Create the search pattern with the lab number
+$searchPattern = "Lab $labNumber Download*.zip"
+
+$zipFiles = Get-ChildItem -Path $downloadsPath -Filter $searchPattern
+
+if ($zipFiles.Count -eq 0) {
+    Write-Host "No zip files found matching 'Lab $labNumber Download' in the Downloads directory." -ForegroundColor Red
+    exit
+}
+
+foreach ($zipFile in $zipFiles) {
+    $destinationPath = [System.IO.Path]::Combine($downloadsPath, [System.IO.Path]::GetFileNameWithoutExtension($zipFile.Name))
+
+    if (Test-Path -Path $destinationPath) {
+        Write-Host "$zipFile.FullName has already been unzipped." -ForegroundColor Red
+        exit
+    }
+
+    New-Item -ItemType Directory -Path $destinationPath | Out-Null
+
+    Expand-Archive -Path $zipFile.FullName -DestinationPath $destinationPath -Force
+    Write-Host "Unzipped '$($zipFile.Name)'" -ForegroundColor Green
+
+    $studentFiles = Get-ChildItem -Path $destinationPath -Filter '*.zip' -File
+    $totalFilesUnzipped = 0
+
+    foreach ($studentFile in $studentFiles){
+        $studentFilePath = [System.IO.Path]::Combine($studentFile.DirectoryName, [System.IO.Path]::GetFileNameWithoutExtension($studentFile.Name))
+
+        if (-Not (Test-Path -Path $studentFilePath)) {
+            New-Item -ItemType Directory -Path $studentFilePath | Out-Null
+        }
+
+        Expand-Archive -Path $studentFile.FullName -DestinationPath $studentFilePath -Force
+        Remove-Item -Path $studentFile.FullName -Force -Recurse
+        $totalFilesUnzipped += 1
+    }
+    Write-Host "Finished unzipping $totalFilesUnzipped student submissions to '$destinationPath'." -ForegroundColor Green
+}
+
